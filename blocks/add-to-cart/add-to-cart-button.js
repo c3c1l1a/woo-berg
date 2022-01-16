a2486d8fa35c09c3d66917b3c148ff20ad2661e4
c3c1l1a <cesswangumukima@gmail.com>
2022-01-16 14:43:32 +0800

    The button triggers an event both on template and on woo-commerce product

--- blocks/add-to-cart/add-to-cart-button.js
+++ blocks/add-to-cart/add-to-cart-button.js
@@ -6,27 +6,30 @@
 		addToCartButton .on('click', function(e){
 			e.preventDefault();
 
-			var data = {
-				action: 'wooberg_ajax_add_to_cart',
-				product_id: $(this).attr('data-product-id'),
-				quantity: 1,
+			if ($(this).attr('data-product-id')){
+				var data = {
+					action: 'wooberg_ajax_add_to_cart',
+					product_id: $(this).attr('data-product-id'),
+					quantity: 1,
+				}
+
+				$.ajax({
+					url: wc_add_to_cart_params.ajax_url,
+					type: 'POST',
+					data: data,
+					success: function() {
+		                $(document.body).trigger('added_to_cart');
+		    		}
+				});
+			} else {
+				$(document.body).trigger('added_to_cart');
 			}
 
-			$.ajax({
-				url: wc_add_to_cart_params.ajax_url,
-				type: 'POST',
-				data: data,
-				success: function() {
-	                $(document.body).trigger('added_to_cart');
-	    		}
-			});
 
-			$(document.body).on( 'added_to_cart', function(event){
-				alert('yeah');
-			});
+		});
 
-
-
+		$(document.body).on( 'added_to_cart', function(event){
+			alert('yeah');
 		});
 
 		
