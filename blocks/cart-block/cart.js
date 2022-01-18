3bc5e138ac1962383d05c59976dc2362de685dde
c3c1l1a <cesswangumukima@gmail.com>
2022-01-18 14:02:33 +0800

    The cart can be toggled on and out

--- blocks/cart-block/cart.js
+++ blocks/cart-block/cart.js
@@ -1,10 +1,34 @@
 ( function($){
 	$(document).ready(function(){
 
+		var wooberg_cart = $('.wooberg-cart');
+
+
 		$(document.body).on('added_to_cart', function(e){
-			console.log('added_to_cart');
+
+			if (wooberg_cart.hasClass('wooberg-cart-slide-out'))
+				wooberg_cart.removeClass('wooberg-cart-slide-out');
+			
+			wooberg_cart.addClass('wooberg-cart-slide-in');
+			wooberg_cart.show();
+			
+		});
+
+		$('.wooberg-cart p:nth-child(2)').on('click', function(){
+			if (wooberg_cart.hasClass('wooberg-cart-slide-in'))
+				wooberg_cart.removeClass('wooberg-cart-slide-in')
+			
+			wooberg_cart.addClass('wooberg-cart-slide-out');
+		
+			setTimeout(function(){
+				wooberg_cart.hide();
+			}, 300);
+			
+			
 		});
 	
 
 	});
-})(jQuery);
+})(
+	jQuery
+);
